3e60a826d76ade865374e332a2ed6cb355c0b33f
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2022-03-27 15:10:43 -0400

    added animations to text

--- project/main.js
+++ project/main.js
@@ -3,6 +3,10 @@ import gsap from "gsap";
 import scrollTo from "gsap/ScrollToPlugin";
 import scrollTrigger from "gsap/ScrollTrigger";
 
+window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
+};
+
 gsap.registerPlugin(scrollTo, scrollTrigger);
 
 gsap.set(".splash", {
@@ -14,7 +18,22 @@ gsap.set(".splash", {
   left: "50%",
   x: "-50%",
 });
+
 gsap.set(".scrollDist", { width: "100%", height: "200%" });
+
+gsap
+  .timeline({ defaults: { duration: 1 } })
+  .from(".explore", { y: -50, opacity: 0, ease: "power2.out" }, 0)
+  .from(
+    ".arrow-down",
+    {
+      y: -25,
+      opacity: 0,
+      ease: "power1.out",
+    },
+    "-=.4"
+  );
+
 gsap
   .timeline({
     scrollTrigger: {
@@ -27,24 +46,24 @@ gsap
   .fromTo(".bg", { y: 0 }, { y: -250 }, 0)
   .fromTo(".cloud1", { y: 100 }, { y: -800 }, 0)
   .fromTo(".cloud2", { y: -150 }, { y: -500 }, 0)
-  .fromTo(".cloud3", { y: -50 }, { y: -575 }, 0);
-
-// document.querySelector("#arrowBtn").addEventListener("scroll", () => {
-//   document.querySelector("#arrowBtn").style.display = "none";
-// });
+  .fromTo(".cloud3", { y: -50 }, { y: -575 }, 0)
+  .to(".explore", { opacity: 0.3 }, 0)
+  .to(".arrow-down", { opacity: 0 }, 0)
+  .from(".iceland", { opacity: 0.3, ease: "power2.out" }, 0)
+  .from(".go", { y: -25, opacity: 0, ease: "power1.out" }, "-=.35");
 
 document.querySelector("#arrowBtn").addEventListener("mouseenter", () => {
   gsap.to(".arrow-down", {
     y: 10,
-    duration: 0.8,
-    ease: "back.inOut(3)",
+    duration: 0.7,
+    ease: "back.inOut(2)",
     overwrite: "auto",
   });
 });
 document.querySelector("#arrowBtn").addEventListener("mouseleave", () => {
   gsap.to(".arrow-down", {
     y: 0,
-    duration: 0.5,
+    duration: 0.6,
     ease: "power3.out",
     overwrite: "auto",
   });
@@ -54,5 +73,22 @@ document.querySelector("#arrowBtn").addEventListener("click", () => {
     scrollTo: innerHeight,
     duration: 1.2,
     ease: "power1.inOut",
+  });
+});
+
+document.querySelector(".go").addEventListener("mouseenter", () => {
+  gsap.to(".go", {
+    y: -5,
+    duration: 0.5,
+    ease: "power3.out",
+    overwrite: "auto",
+  });
+});
+document.querySelector(".go").addEventListener("mouseleave", () => {
+  gsap.to(".go", {
+    y: 0,
+    duration: 0.25,
+    ease: "power2.out",
+    overwrite: "auto",
   });
 });
